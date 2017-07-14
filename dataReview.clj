69493b98208406f7cd91c4230058079ca37bdaf8
Andy Haddon <lastcanti@gmail.com>
2017-07-13 21:13:26 -0400

    Add files via upload

---
+++ dataReview.clj
@@ -0,0 +1,27 @@
+;  review of simple datatypes
+; strings, integers, ratios, decimals, keywords,
+; characters, and booleans
+
+; Lists, 
+
+; Use data in functions or expressions.
+; Lists: when you want access from top 
+'("item" "thing" 1/3)
+
+; Vectors: when you need index access(array)
+["a" "b" "c"]
+
+; Maps: when you need key-value pairs easily organized
+{:hero "batman" :hero1 "superman" :hero2 "vegeta"}
+
+; Sets: Unique data results by grouping elements
+#{:a :b :c}
+
+; Use def to create a new function
+(def developer "AndyPie")
+
+; Use let to allow binding of symbols only within context
+; "What happens in a let stays in a let"
+(let [developer "AndrewWilliam"]
+     developer)
+   
